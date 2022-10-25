8a30137190279609e07e67efb852582c75970250
ComRV <r.duika.9d@gmail.com>
2022-10-25 21:46:38 +0700

    fix feedback login

--- middleware.js
+++ middleware.js
@@ -4,5 +4,10 @@ export default function middleware(req) {
 	if (req.nextUrl.pathname.startsWith('/chat') && req.cookies.get('_AT') === undefined) {
 		return NextResponse.redirect(new URL('/', req.url));
 	}
+
+	if ((req.nextUrl.pathname === '/' || req.nextUrl.pathname === '/register') && req.cookies.get('_AT') !== undefined) {
+		return NextResponse.redirect(new URL('/chat', req.url));
+	}
+
 	return NextResponse.next();
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -3,18 +3,29 @@ import Head from 'next/head';
 import Link from 'next/link';
 import { useState } from 'react';
 import axios from 'axios';
+import Router from 'next/router';
+import ReactLoading from 'react-loading';
 
 export default function Home() {
 	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
 	const [isStay, setIsStay] = useState(false);
+	const [isSuccess, setIsSuccess] = useState('');
+	const [isLoading, setIsLoading] = useState(false);
+	const [msg, setMsg] = useState('');
 
 	const auth = async (event) => {
 		event.preventDefault();
 		try {
+			setIsLoading(true);
 			const auth = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth`, { username, password, isStay });
+			setIsLoading(false);
+			Router.push('/chat');
 		} catch (error) {
-			console.log(error);
+			setIsLoading(false);
+			const response = error.response.data;
+			setMsg(response.msg);
+			setIsSuccess(response.status);
 		}
 	};
 
@@ -24,10 +35,11 @@ export default function Home() {
 				<title>P - Login</title>
 			</Head>
 			<Auth>
-				<div className="text-white container w-2/3 rounded-md h-[9.8cm] m-auto border border-zinc-800 bg-[#212121] md:w-2/4 lg:w-1/3">
+				<div className="text-white container w-2/3 rounded-md h-[10.6cm] m-auto border border-zinc-800 bg-[#212121] md:w-2/4 lg:w-1/3">
 					<p className="text-center mt-10 font-roboto font-semibold text-sm text-zinc-600">WELCOME BACK</p>
 					<p className="text-center text-zinc-200 font-bold text-xl">LOGIN INTO YOUR ACCOUNT</p>
-					<form className="mt-6" onSubmit={auth}>
+					{!isSuccess && msg && <div className="py-2 px-3 mt-3 w-[90%] m-auto text-sm text-red-700 bg-red-100 rounded-md">{msg}</div>}
+					<form className="mt-3" onSubmit={auth}>
 						<div className="flex flex-col">
 							<label className="font-intertight tracking-wider text-sm text-zinc-300 ml-[5%]">Username</label>
 							<input
@@ -62,10 +74,10 @@ export default function Home() {
 						<div className="flex flex-col">
 							<button
 								type="submit"
-								className="bg-[#0d7477] w-[90%] m-auto h-[35px] rounded hover:bg-[#0d7477e0] active:bg-[#0d7477c0] duration-150 font-intertight tracking-wider text-sm font-medium"
-								required="true"
+								className="bg-[#0d7477] flex items-center justify-center w-[90%] gap-x-1 m-auto h-[35px] rounded hover:bg-[#0d7477e0] active:bg-[#0d7477c0] duration-150 font-intertight tracking-wider text-sm font-medium"
 							>
-								Sign In
+								{isLoading && <ReactLoading type="spinningBubbles" color="#ffffff" height={'17px'} width={'17px'} />}
+								<p>Sign In</p>
 							</button>
 							<p className="font-intertight tracking-wider font-zinc-200 text-xs ml-[5%] mt-1">
 								Dont have an account?
